f9c41577dd157b0c2377caac9126191bfc8019e4
Yugandhar Reddy <yugandharreddy-03@users.noreply.github.com>
2026-03-14 23:25:41 +0530

    updated

--- Message/src/Info.java
+++ Message/src/Info.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Info {
     public static void main(String[] args) {
-      System.out.println("good ngt");
+      System.out.println("good ");
     }
 }
